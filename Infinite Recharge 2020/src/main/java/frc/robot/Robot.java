caa86c39bbf78c0df84a33e0d031be2462439d03
Sequoyah FRC <sequoyahfrc@gmail.com>
2020-01-30 07:59:26 -0500

    motor controllers (4 VictorSPX's 4 Talon SRX's)

--- Infinite Recharge 2020/src/main/java/frc/robot/Robot.java
+++ Infinite Recharge 2020/src/main/java/frc/robot/Robot.java
@@ -6,13 +6,41 @@ import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.*;
+
 public class Robot extends TimedRobot {
   private DifferentialDrive _robot;
   private XboxController driver1;
 
+  //motor controllers
+  private VictorSPX masterLeft;
+  private VictorSPX slaveLeft;
+  private VictorSPX masterRight;
+  private VictorSPX slaveRight;
+  private TalonSRX talon1;
+  private TalonSRX talon2;
+  private TalonSRX talon3;
+  private TalonSRX talon4;
+
   @Override
   public void robotInit() {
-    _robot = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+
+    //TODO: ask shane what ports things go in
+    //left motor controllers for drive
+    masterLeft = new VictorSPX(0);
+    slaveLeft = new VictorSPX(1);
+    //right motor controllers for drive
+    masterRight = new VictorSPX(2);
+    slaveRight = new VictorSPX(3);
+    //talons
+    talon1 = new TalonSRX(4);
+    talon2 = new TalonSRX(5);
+    talon3 = new TalonSRX(6);
+    talon4 = new TalonSRX(7);
+    //robot
+    _robot = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1)); //TODO: change this to support our Victor SPX
+    //controllers
     driver1 = new XboxController(0);
   }
 
