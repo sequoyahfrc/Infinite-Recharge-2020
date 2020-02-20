f4abd9a8b7d3c7060f85230c1e849d57f1cfcb15
Sequoyah FRC <sequoyahfrc@gmail.com>
2020-02-20 10:14:35 -0500

    added auto intake back using doublesolenoid class

--- Infinite Recharge 2020/src/main/java/frc/robot/Robot.java
+++ Infinite Recharge 2020/src/main/java/frc/robot/Robot.java
@@ -22,6 +22,8 @@ public class Robot extends TimedRobot {
   //constants
   private final double INTAKE_AIR_PULSE_TIME = 0.3;
   private final double INTAKE_SPEED = 0.75;
+  private final double SHOOTER_SPEED = 1.0;
+
   //robot
   private DifferentialDrive _robot;
 
@@ -35,7 +37,6 @@ public class Robot extends TimedRobot {
   private SpeedControllerGroup mGroupLeft;
   private SpeedControllerGroup mGroupRight;
 
-  //other motors TODO: refactor the talons for what they are used for
   private WPI_TalonSRX intakeMotor;
   private WPI_TalonSRX conveyorMotor;
   private WPI_TalonSRX shooterMotorL;
@@ -53,8 +54,8 @@ public class Robot extends TimedRobot {
   private final Timer extTimer = new Timer();
   private final Timer retTimer = new Timer();
 
-  //prevent breaking intake code
-  private final boolean canIntake = true;
+  //other
+  boolean canIntake = true;
 
   @Override
   public void robotInit() {
@@ -70,18 +71,20 @@ public class Robot extends TimedRobot {
     mGroupLeft = new SpeedControllerGroup(masterLeft, slaveLeft);
     mGroupRight = new SpeedControllerGroup(masterRight, slaveRight);
     // talons
-    intakeMotor = new WPI_TalonSRX(0);
-    conveyorMotor = new WPI_TalonSRX(1);
-    shooterMotorL = new WPI_TalonSRX(2); // shooter motor left?
-    shooterMotorR = new WPI_TalonSRX(3); // shooter motor right?
+    intakeMotor = new WPI_TalonSRX(4);
+    conveyorMotor = new WPI_TalonSRX(5);
+    shooterMotorL = new WPI_TalonSRX(6); // shooter motor left?
+    shooterMotorR = new WPI_TalonSRX(7); // shooter motor right?
     // pneumatics
     _compressor = new Compressor();
-    intakeSol = new DoubleSolenoid(60,0, 1);
+    intakeSol = new DoubleSolenoid(60, 0, 1);
     // robot
     _robot = new DifferentialDrive(mGroupLeft, mGroupRight);
     // controllers
     driver1 = new XboxController(0);
     driver2 = new XboxController(1);
+    //invert shooter left because it goes counter clockwise
+    shooterMotorL.setInverted(true);
   }
 
   @Override
@@ -92,65 +95,54 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    //drive
     final double left = driver1.getY(Hand.kLeft);
     final double right = driver1.getY(Hand.kRight);
     _robot.tankDrive(left, right);
+
     // intake
-    intakeMotor.set(driver1.getAButton() ? INTAKE_SPEED : 0);
-    
     if (driver1.getBumper(Hand.kRight)) {
-      intakeSol.set(Value.kForward);
-      /*
-       * if (canIntake) { //when bumper not pressed on the last check but is on the
-       * current check if (driver1.getBumperPressed(Hand.kRight)) { extTimer.start();
-       * extendIntake.set(true); } intakeMotor.set(INTAKE_SPEED); canIntake = false;
-       * //disable intake }
-       */
-      
-    }
-    if(driver1.getXButton()){
-      intakeSol.set(Value.kReverse);
-    }
-    else if(driver1.getYButton()){
-      intakeSol.set(Value.kForward);
-    }
-    else{
-     intakeSol.set(Value.kOff);
+      if (canIntake) {
+        extTimer.start();
+        intakeSol.set(Value.kForward);
+        intakeMotor.set(INTAKE_SPEED);
+        canIntake = false;
+      }
     }
 
+    //shooter
+    if (driver1.getXButton()) { // should i move this to driver 2
+      shooterMotorR.set(SHOOTER_SPEED);
+      shooterMotorL.set(SHOOTER_SPEED);
+    } else {
+      shooterMotorR.stopMotor();
+      shooterMotorL.stopMotor();
+    }
 
-    /*if (driver1.getRawButtonPressed(4)) {
-    *  intakeSol.set(DoubleSolenoid.Value.kReverse);
-   * }
-    *else {
-    *  intakeSol.set(DoubleSolenoid.Value.kForward);
-    *}
-    */
-    // after extTimer reaches INTAKE_AIR_PULSE_TIME
-    /*
-     * if (extTimer.hasPeriodPassed(INTAKE_AIR_PULSE_TIME)) {
-     * extendIntake.set(false); //when bumper released if
-     * (driver1.getBumperReleased(Hand.kRight)) { retTimer.start();
-     * retractIntake.set(false); extTimer.stop(); extTimer.reset(); } } //after
-     * retTimer reaches INTAKE_AIR_PULSE_TIME if
-     * (retTimer.hasPeriodPassed(INTAKE_AIR_PULSE_TIME)) { retTimer.stop();
-     * retTimer.reset(); retractIntake.set(true); intakeMotor.stopMotor(); canIntake
-     * = true; //you can use intake again }
-     */
-    // conveyor
+    //after extTimer reaches INTAKE_AIR_PULSE_TIME
+    if (extTimer.hasPeriodPassed(INTAKE_AIR_PULSE_TIME)) {
+      if (driver1.getBumperReleased(Hand.kRight)) {
+        extTimer.stop();
+        extTimer.reset();
+        retTimer.start();
+        intakeSol.set(Value.kReverse);
+      }
+    }
+
+    //after retTimer reaches INTAKE_AIR_PULSE_TIME
+    if (retTimer.hasPeriodPassed(INTAKE_AIR_PULSE_TIME)) {
+      retTimer.stop();
+      retTimer.reset();
+      canIntake = true;
+      intakeSol.set(Value.kOff);
+      intakeMotor.stopMotor();
+    }
+
+    //conveyor
     if (driver1.getBButton()) { // should i move this to driver 2
       conveyorMotor.set(INTAKE_SPEED);
     } else {
       conveyorMotor.stopMotor();
-    }
-    // shooter
-    final double triggerR = (driver1.getRawAxis(5) + 1) / 2; // trigger goes from -1 to 1
-    if (triggerR > 0) {
-      shooterMotorL.set(triggerR);
-      shooterMotorR.set(triggerR);
-    } else {
-      shooterMotorL.stopMotor();
-      shooterMotorR.stopMotor();
     }
   }
 } 
