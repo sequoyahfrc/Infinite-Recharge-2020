b7799cf020959a6b26a94da7cb26179eb6554f4f
Sequoyah FRC <sequoyahfrc@gmail.com>
2020-02-24 17:18:16 -0500

    more comments

--- Infinite Recharge 2020/src/main/java/frc/robot/MotorButtonBinding.java
+++ Infinite Recharge 2020/src/main/java/frc/robot/MotorButtonBinding.java
@@ -3,17 +3,19 @@ package frc.robot;
 import edu.wpi.first.wpilibj.SpeedController;
 
 class MotorButtonBinding {
+
+    //should ONLY be set in the constructor, and not modified in ANY WAY
     private final SpeedController[] motors;
     private double speed;
 
-    //"..." means you can pass as many values as you want for one argument, its stored as an array
-    //                                                                  ↓
+    //"..." means you can pass as many values as you want for one argument, it is stored as an array
     public MotorButtonBinding(double speed, SpeedController... motors) {
         this.motors = motors;
         this.speed = speed;
     }
 
     //if expression is true, start the motor at a certain speed specified in the constructor
+    //you can pass driver1.getXButton() to bind all of the motors in motors array (see above) to the X button
     public void SetMotors(boolean expression) {
         for (SpeedController motor : motors) {
             if (expression) {
@@ -24,7 +26,9 @@ class MotorButtonBinding {
         }
     }
 
+    //use this to change the speed in case you want to change the speed
+    //APPLIES TO ALL MOTORS IN THE MOTORS ARRAY!!!
     public void SetSpeed(double speed) {
-        this. speed = speed;
+        this.speed = speed;
     }
 }
