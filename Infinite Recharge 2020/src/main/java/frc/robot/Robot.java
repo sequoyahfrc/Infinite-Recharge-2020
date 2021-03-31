4922253474837d870a7b73a2560b9675f1256fab
Sequoyah FRC <sequoyahfrc@gmail.com>
2021-03-31 16:04:06 -0400

    shooter is not programmed

--- Infinite Recharge 2020/src/main/java/frc/robot/Robot.java
+++ Infinite Recharge 2020/src/main/java/frc/robot/Robot.java
@@ -2,8 +2,6 @@ package frc.robot;
 
 //WPILIB imports
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 //CTRE imports
@@ -16,10 +14,6 @@ import java.util.*;
 public class Robot extends TimedRobot {
 
   // --------------------CONSTANTS--------------------
-  private final double INTAKE_SPEED = 0.75;
-  private final double SHOOTER_SPEED = 1.0;
-  private final double CONVEYOR_SPEED = -0.6;
-  private int REVERSE = 1;
 
   // robot
   private DifferentialDrive robot;
@@ -29,11 +23,11 @@ public class Robot extends TimedRobot {
   private WPI_VictorSPX slaveLeft;
   private WPI_VictorSPX masterRight;
   private WPI_VictorSPX slaveRight;
-  private WPI_TalonSRX intakeMotor; // TODO: determine if used
-  private WPI_TalonSRX conveyorMotor; // TODO: determine if used
-  private WPI_TalonSRX shooterMotorL; // TODO: determine if used
-  private WPI_TalonSRX shooterMotorR; // TODO: determine if used
-  private WPI_VictorSPX conveyorFront; // TODO: determine if used
+  private WPI_TalonSRX intakeMotor;
+  private WPI_TalonSRX conveyorMotor;
+  private WPI_VictorSPX shooterMotor;
+  private WPI_TalonSRX climbMotor1;
+  private WPI_TalonSRX climbMotor2;
 
   // speed controller groups
   private SpeedControllerGroup mGroupLeft;
@@ -45,62 +39,58 @@ public class Robot extends TimedRobot {
 
   // pneumatics
   private Compressor compressor; // TODO: determine if used
-  private DoubleSolenoid intakeSol; // TODO: determine if used 
-  private DoubleSolenoid stopperSol; // TODO: determine if used
 
   // Handlers
   private final ArrayList<IRobotEventHandler> EVENT_HANDLERS = new ArrayList<>();
 
   @Override
   public void robotInit() {
-    // victors are for drive system, talons are for accessories
     // rear motors are slaves front motors are masters
-    // left motor controllers for drive
+    // Drive motors
     masterLeft = new WPI_VictorSPX(0);
     slaveLeft = new WPI_VictorSPX(1);
-    // right motor controllers for drive
     masterRight = new WPI_VictorSPX(2);
     slaveRight = new WPI_VictorSPX(3);
     // speed controllers
     mGroupLeft = new SpeedControllerGroup(masterLeft, slaveLeft);
     mGroupRight = new SpeedControllerGroup(masterRight, slaveRight);
-    // talons
-    intakeMotor = new WPI_TalonSRX(4); // TODO: determine if used
-    conveyorMotor = new WPI_TalonSRX(5); // TODO: determine if used
-    shooterMotorL = new WPI_TalonSRX(6); // shooter motor left // TODO: determine if used
-    shooterMotorR = new WPI_TalonSRX(7); // shooter motor right // TODO: determine if used
-    conveyorFront = new WPI_VictorSPX(8); // TODO: determine if used
-    // pneumatics
-    compressor = new Compressor(); // TODO: determine if used
-    intakeSol = new DoubleSolenoid(60, 0, 1); // TODO: determine if used
-    stopperSol = new DoubleSolenoid(60, 2, 3); // TODO: determine if used
     // robot
     robot = new DifferentialDrive(mGroupLeft, mGroupRight);
+    // other motors
+    intakeMotor = new WPI_TalonSRX(7);
+    conveyorMotor = new WPI_TalonSRX(5);
+    shooterMotor = new WPI_VictorSPX(8);
+    climbMotor1 = new WPI_TalonSRX(4);
+    climbMotor2 = new WPI_TalonSRX(6);
+    // pneumatics
+    compressor = new Compressor(); // TODO: determine if used
     // controllers
     driver1 = new XboxController(0);
     driver2 = new XboxController(1);
-    // invert shooter left because it goe s counter clockwise
-    shooterMotorR.setInverted(true);
 
     // Event Handlers
     EVENT_HANDLERS.add(new CompressorHandler(compressor));
     EVENT_HANDLERS.add(new GoForward(robot));
     EVENT_HANDLERS.add(new TankDriveHandler(robot, driver2));
     EVENT_HANDLERS.add(new LimeLightHandler(robot, driver1));
+    EVENT_HANDLERS.add(new ShooterHandler(driver1, shooterMotor));
 
     EVENT_HANDLERS.forEach(IRobotEventHandler::robotInit);
   }
 
+  // only exectes when disabled, use for cleanup
   @Override
   public void disabledInit() {
     EVENT_HANDLERS.forEach(IRobotEventHandler::disabledInit);
   }
 
+  // only executes once when autonomous starts
   @Override
   public void autonomousInit() {
     EVENT_HANDLERS.forEach(IRobotEventHandler::autonomousInit);
   }
 
+  // loops over itself (every ~.02 seconds) until disabled
   @Override
   public void autonomousPeriodic() {
     EVENT_HANDLERS.forEach(IRobotEventHandler::autonomousPeriodic);
@@ -116,35 +106,5 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     EVENT_HANDLERS.forEach(IRobotEventHandler::teleopPeriodic);
-    // TODO: Determine if this is used
-    if (driver1.getBumperPressed(Hand.kLeft)) {
-      REVERSE *= -1;
-    }
-    if (driver1.getBButton()) {
-      stopperSol.set(Value.kForward);
-    } else {
-      stopperSol.set(Value.kReverse);
-    }
-    if (driver1.getAButton()) {
-      intakeSol.set(Value.kForward);
-      intakeMotor.set(INTAKE_SPEED * REVERSE);
-      conveyorFront.set(INTAKE_SPEED * REVERSE);
-    } else {
-      intakeSol.set(Value.kReverse);
-      intakeMotor.stopMotor();
-      conveyorFront.stopMotor();
-    }
-    if (driver1.getYButton()) {
-      shooterMotorL.set(SHOOTER_SPEED);
-      shooterMotorR.set(SHOOTER_SPEED);
-    } else {
-      shooterMotorL.stopMotor();
-      shooterMotorR.stopMotor();
-    }
-    if (driver1.getXButton()) {
-      conveyorMotor.set(CONVEYOR_SPEED);
-    } else {
-      conveyorMotor.stopMotor();
-    }
   }
 }
---
+++ Infinite Recharge 2020/src/main/java/frc/robot/handlers/ShooterHandler.java
@@ -0,0 +1,25 @@
+package frc.robot.handlers;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
+public class ShooterHandler implements IRobotEventHandler {
+
+  private final WPI_VictorSPX shooterMotor;
+  private final XboxController driver1;
+
+  public ShooterHandler(XboxController driver1, WPI_VictorSPX shooterMotor) {
+    this.driver1 = driver1;
+    this.shooterMotor = shooterMotor;
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double trigger = driver1.getTriggerAxis(Hand.kRight);
+    if (trigger > 0) {
+      shooterMotor.set(trigger);
+    }
+  }
+  
+}
