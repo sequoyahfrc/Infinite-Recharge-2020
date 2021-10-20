b9ea3a20b8800203f931ec24e2dc91cc041b05bc
Sequoyah FRC <sequoyahfrc@gmail.com>
2021-10-20 15:32:36 -0400

    suppressing warnings

--- Infinite Recharge/src/main/java/frc/robot/Robot.java
+++ Infinite Recharge/src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command autonomousCommand;
+  @SuppressWarnings("unused")
   private RobotContainer robotContainer;
 
   @Override
