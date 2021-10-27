b8300fedb5f9ebd1da966a2df0c6edceda6526c0
Sequoyah FRC <sequoyahfrc@gmail.com>
2021-10-27 14:58:08 -0400

    intake limit switch

--- Infinite Recharge/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ Infinite Recharge/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,11 +2,15 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class IntakeSubsystem extends SubsystemBase {
 
   private final WPI_TalonSRX intake, towerL, towerR;
+  private final DigitalInput stopIntakeSwitch;
 
   public IntakeSubsystem() {
     intake = new WPI_TalonSRX(4);
@@ -14,17 +18,32 @@ public class IntakeSubsystem extends SubsystemBase {
     towerR = new WPI_TalonSRX(7);
     towerR.setInverted(true);
     towerL = new WPI_TalonSRX(5);
+    stopIntakeSwitch = new DigitalInput(0); //TODO: change ID
   }
 
   public void setSpeed(double value) {
     intake.set(value);
-    towerL.set(value * 0.75);
-    towerR.set(value * 0.75);
   }
 
-  public void stop() {
+  public void setTower(double value) {
+    towerL.set(value);
+    towerR.set(value);
+  }
+
+  public void stopIntake() {
     intake.stopMotor();
+  }
+
+  public void stopTower() {
     towerL.stopMotor();
     towerR.stopMotor();
+  }
+
+  public boolean getStopIntakeSwitchValue() {
+    return stopIntakeSwitch.get();
+  }
+
+  public Trigger getStopIntakeSwitch() {
+    return new Trigger(this::getStopIntakeSwitchValue);
   }
 }
