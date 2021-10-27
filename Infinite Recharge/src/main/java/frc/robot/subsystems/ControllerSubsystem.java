7d5749cd109698a28fe01294d430974cc6234009
Sequoyah FRC <sequoyahfrc@gmail.com>
2021-10-27 15:02:36 -0400

    comments

--- Infinite Recharge/src/main/java/frc/robot/subsystems/ControllerSubsystem.java
+++ Infinite Recharge/src/main/java/frc/robot/subsystems/ControllerSubsystem.java
@@ -2,7 +2,19 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.XboxController;
 
-public class ControllerSubsystem {
+/*
+While the ControllerSubsystem is a Subsystem,
+each subsystem cann only be used by one command
+at a time. If two commands need the Controllers and
+this class is a a SubsystemBase passed to addRequirements(),
+then only one command will run.
+
+TLDR:
+  - DO NOT UNCOMMENT "extends SubsystemBase"!!!!!!!!
+  - DO NOT PASS THIS TO addRequirements()!!!!!!!!!!!!!
+OR ELSE THINGS *WILL* BREAK!
+*/
+public class ControllerSubsystem /*extends SubsystemBase*/{
 
   private final XboxController driver1;
   private final XboxController driver2;
